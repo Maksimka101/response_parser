d529043372aef6cd32514cd5560513451565d862
Maksim Zemlyanikin <zemlyanikin.maksim@gmail.com>
2023-01-19 02:10:20 +0400

    Update changelog and example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 - Add `ResponseParserBase` class which can be extended to create custom response parser.
 - Add a diagram to the readme.
 - Update `fpdart` dependency version to the `0.4.0`.
+- Write tests.
 
 ## 1.0.0+1
 
--- example/lib/example.dart
+++ example/lib/example.dart
@@ -49,7 +49,7 @@ Future<void> main() async {
   );
 
   final usersResult = await parseListApiResponse(
-    requestAction: () => dio.get('/user'),
+    requestAction: () => dio.get('/users'),
     mapper: User.fromJson,
   );
   usersResult.fold(
@@ -60,7 +60,7 @@ Future<void> main() async {
   final deleteUserResult = await parseEmptyApiResponse(
     requestAction: () => dio.delete('/user'),
   );
-  deleteUserResult.match(
+  deleteUserResult.match<void>(
     (failure) => print('Error: ${failure.displayError}'),
     () => print('User is successfully deleted!'),
   );
